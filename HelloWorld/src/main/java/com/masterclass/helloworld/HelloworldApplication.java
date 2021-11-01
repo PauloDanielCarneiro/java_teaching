eeb4f5bb75632f875ede34e4b86422ac00f3a538
Paulo Daniel <pdc187@gmail.com>
2021-11-01 00:05:57 -0300

    feat: Add primitive type examples

--- HelloWorld/src/main/java/com/masterclass/helloworld/HelloworldApplication.java
+++ HelloWorld/src/main/java/com/masterclass/helloworld/HelloworldApplication.java
@@ -5,8 +5,13 @@ public class HelloworldApplication {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 
-		int myNumber = 5;
-		System.out.println(myNumber);
+		int myFirstNumber = (10 + 5) + (2 * 10);
+		int mySecondNumber = 12;
+		int myThirdNumber = myFirstNumber * 2;
+		int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+		System.out.println(myTotal);
+		int myLastOne = 1000 - myTotal;
+		System.out.println(myLastOne);
 	}
 
 }
