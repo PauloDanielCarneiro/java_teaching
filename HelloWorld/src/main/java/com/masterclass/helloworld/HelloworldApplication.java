27f7e7ecced30bd8ded4b707286ab67220614a70
Paulo Daniel <pdc187@gmail.com>
2021-10-31 23:53:23 -0300

    Add first commit

---
+++ HelloWorld/src/main/java/com/masterclass/helloworld/HelloworldApplication.java
@@ -0,0 +1,12 @@
+package com.masterclass.helloworld;
+
+public class HelloworldApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+		int myNumber = 5;
+		System.out.println(myNumber);
+	}
+
+}
---
+++ readme.md
@@ -0,0 +1,4 @@
+#Java master class
+[this course](https://www.udemy.com/course/java-the-complete-java-developer-course)
+
+This is used to help some friends to learn programming
