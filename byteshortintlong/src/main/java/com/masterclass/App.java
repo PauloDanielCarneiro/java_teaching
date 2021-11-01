4e48b1707cc0791d4773104f2611851120777e77
Paulo Daniel <pdc187@gmail.com>
2021-11-01 00:15:52 -0300

    Add types class explanation

--- byteshortintlong/src/main/java/com/masterclass/App.java
+++ byteshortintlong/src/main/java/com/masterclass/App.java
@@ -4,10 +4,26 @@ package com.masterclass;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        int maxValue = Integer.MAX_VALUE;
+        int minValue = Integer.MIN_VALUE;
+        System.out.println("max int: " + maxValue);
+        System.out.println("min int: " + minValue);
+
+        byte maxByteValue = Byte.MAX_VALUE;
+        byte minByteValue = Byte.MIN_VALUE;
+        System.out.println("max byte: " + maxByteValue);
+        System.out.println("min byte: " + minByteValue);
+
+        short maxShortValue = Short.MAX_VALUE;
+        short minShortValue = Short.MIN_VALUE;
+        System.out.println("max short: " + maxShortValue);
+        System.out.println("min short: " + minShortValue);
+
+        long maxLongValue = Long.MAX_VALUE;
+        long minLongValue = Long.MIN_VALUE;
+        System.out.println("max long: " + maxLongValue);
+        System.out.println("min long: " + minLongValue);
     }
 }
